607c1969dda89c570e0f61e71d810a65aa6e5969
Enrique Gomez <enrique.gomez.electronico@gmail.com>
2020-02-04 20:06:48 +0100

    Service

--- bash/install.sh
+++ bash/install.sh
@@ -18,3 +18,19 @@ echo "* * * * * curl http://127.0.0.1/cron/" >> copy
 #Save the cron file
 crontab copy
 rm copy
+
+sudo cp configuration_templates/homeware.service /lib/systemd/system/
+
+#sudo systemctl start homeware
+#sudo systemctl stop homeware
+#sudo systemctl status homeware
+
+#Get current sudo crontab
+sudo crontab -l > copy
+#Set the new cron job up
+echo "@reboot sudo systemctl start homeware" >> copy
+#Save the cron file
+sudo crontab copy
+rm copy
+
+sudo systemctl start homeware
