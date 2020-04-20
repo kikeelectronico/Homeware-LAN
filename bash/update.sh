8213843f0464440fdf0d47c720870f64b12eb589
Enrique Gomez <enrique.gomez.electronico@gmail.com>
2020-04-20 23:46:06 +0200

    Reorganice files

--- bash/update.sh
+++ bash/update.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+
+exec 1>upgrader.log 2>&1
+
+echo "The upgrader has started.\r\n"
+
+#Pull from the repository
+git pull
+
 if ! test -f installations.txt; then
     echo "v0.5.1" >> installations.txt
     echo "\r\nThe installations file has been created.\r\n"
@@ -8,7 +17,6 @@ then
   #Intall the new services
   sudo cp ../configuration_templates/homeware.service /lib/systemd/system/
   sudo cp ../configuration_templatesconfiguration_templates/homewareMQTT.service /lib/systemd/system/
-  sudo cp ../configuration_templatesconfiguration_templates/homewareUpgrader.service /lib/systemd/system/
 
   #Get current sudo crontab
   sudo crontab -l > copy
@@ -20,3 +28,10 @@ then
   echo "v0.5.2\r\n" >> installations.txt
   echo "v0.5.2 dependencies have been installed.\r\n"
 fi
+
+
+#Start both services
+sudo systemctl restart homewareMQTT
+sudo systemctl restart homeware
+
+echo "\r\The upgrader has finished.\r\n"
