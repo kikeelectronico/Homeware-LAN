d1a40f2c4c0f4332769d72fe1c40548598287d22
Enrique Gomez <enrique.gomez.electronico@gmail.com>
2020-04-20 23:37:44 +0200

    another test

--- homewareUpgrader.sh
+++ homewareUpgrader.sh
@@ -5,8 +5,8 @@ exec 1>upgrader.log 2>&1
 echo "The upgrader has started.\r\n"
 
 #Stop both services
-sudo systemctl stop homewareMQTT
-sudo systemctl stop homeware
+# sudo systemctl stop homewareMQTT
+# sudo systemctl stop homeware
 
 #Pull from the repository
 git pull
@@ -14,9 +14,9 @@ git pull
 sudo sh upgraderInstructions.sh
 
 #Start both services
-sudo systemctl start homewareMQTT
-sudo systemctl start homeware
+sudo systemctl restart homewareMQTT
+sudo systemctl restart homeware
 
 echo "\r\The upgrader has finished.\r\n"
 #Stop the upgrader
-sudo systemctl stop homewareUpgrader
+# sudo systemctl stop homewareUpgrader
