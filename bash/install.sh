272f816ca282673ef9074cce3fb708db536e06a0
Néstor Chávez <68871748+Ntranx@users.noreply.github.com>
2020-07-27 17:55:29 -0600

    Add files via upload
    Install complete

--- bash/install.sh
+++ bash/install.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
 
 sudo apt-get update
+sudo chown -R root /home/pi
 sudo apt-get install python3-pip
-pip3 install flask
-pip3 install gunicorn
-pip3 install paho-mqtt
+sudo pip3 install flask
+sudo pip3 install gunicorn
+sudo pip3 install paho-mqtt
 sudo apt-get install nginx
 sudo apt-get install software-properties-common
 sudo apt-get install certbot python-certbot-nginx
 sudo apt-get install curl
 sudo apt-get install mosquitto mosquitto-clients
 
-# sudo cp configuration_templates/homeware.service /lib/systemd/system/
+sudo cp configuration_templates/homeware.service /lib/systemd/system/
 
 #sudo systemctl start homeware
-#sudo systemctl stop homeware
+sudo systemctl stop homeware
 #sudo systemctl status homeware
 
 # #Get current sudo crontab
-# sudo crontab -l > copy
+ sudo crontab -l > copy
 # #Set the new cron job up
-# echo "@reboot sudo systemctl start homeware" >> copy
+ echo "@reboot sudo systemctl start homeware" >> copy
 # #Save the cron file
-# sudo crontab copy
-# rm copy
+sudo crontab copy
+ rm copy
 
 sudo sh bash/update.sh
 
