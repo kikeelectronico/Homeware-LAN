78ac62235991064a0db01b4639a3e1265c2f1a68
Enrique Gomez <enrique.gomez.electronico@gmail.com>
2020-08-16 20:30:12 +0200

    Directory correction and boot bugs

--- bash/correctDirectory.sh
+++ bash/correctDirectory.sh
@@ -1,17 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-echo "installing Homeware-LAN.\r\n"
-
-sudo apt-get update
-sudo apt-get install python3-pip
-pip3 install flask
-pip3 install gunicorn
-pip3 install paho-mqtt
-sudo apt-get install nginx
-sudo apt-get install software-properties-common
-sudo apt-get install certbot python-certbot-nginx
-sudo apt-get install curl
-sudo apt-get install mosquitto mosquitto-clients
+echo "Correcting the installation\r\n"
 
 #Intall the new services
 sudo cp configuration_templates/homeware.service /lib/systemd/system/
@@ -19,24 +8,24 @@ sudo cp configuration_templates/homewareMQTT.service /lib/systemd/system/
 sudo cp configuration_templates/homewareTasks.service /lib/systemd/system/
 sudo cp configuration_templates/homewareRedis.service /lib/systemd/system/
 
-#Install redis
-sudo pip3 install redis
-sudo mkdir redis
-cd redis
-wget http://download.redis.io/redis-stable.tar.gz
-tar xvzf redis-stable.tar.gz
-cd redis-stable
-sudo make
-sudo make install
-
 #Eneable the services
 sudo systemctl enable homewareMQTT
 sudo systemctl enable homewareTasks
 sudo systemctl enable homewareRedis
 sudo systemctl enable homeware
 
+#Reload the daemon
+sudo systemctl daemon-reload
+
 #Start the services
 sudo systemctl start homewareMQTT
 sudo systemctl start homewareTasks
 sudo systemctl start homewareRedis
 sudo systemctl start homeware
+
+#Last manual step
+echo "Please run: - sudo crontab -e - and delete the following lines:\r\n"
+echo "@reboot sudo systemctl start homeware\r\n"
+echo "@reboot sudo systemctl start homewareMQTT\r\n"
+echo "@reboot sudo systemctl start homewareTasks\r\n"
+echo "@reboot sudo systemctl start homewareRedis\r\n"
