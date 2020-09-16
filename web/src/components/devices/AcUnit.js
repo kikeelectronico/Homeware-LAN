b3197e7ee7806b8a4f45c3a7bb6e7b7259a28dc4
Enrique Gomez <enrique.gomez.electronico@gmail.com>
2020-09-16 22:00:10 +0200

    Temperature settings at devices list

--- web/src/components/devices/AcUnit.js
+++ web/src/components/devices/AcUnit.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import OnOff from './traits/OnOff'
+import TemperatureSetting from './traits/TemperatureSetting'
 import Information from './aux/Information'
 import Edit from './aux/Edit'
 
@@ -15,11 +16,38 @@ class AcUnit extends React.Component {
       backgroundColor: 'white'
     }
 
+    const traits_container = {
+      paddingLeft: '50px'
+    }
+
+    var color = 'yellow';
+    if (this.props.status.thermostatMode === 'off')
+      color = "#666";
+    else if (this.props.status.thermostatMode === 'heat')
+      color = "red";
+    else if (this.props.status.thermostatMode === 'cool')
+      color = "lightblue";
+    else if (this.props.status.thermostatMode === 'on')
+      color = "yellow";
+    else if (this.props.status.thermostatMode === 'heatcool')
+      color = "#DC97FF";
+    else if (this.props.status.thermostatMode === 'auto')
+      color = "#EE357E";
+    else if (this.props.status.thermostatMode === 'fan-only')
+      color = "blue";
+    else if (this.props.status.thermostatMode === 'purifier')
+      color = "#F3882E";
+    else if (this.props.status.thermostatMode === 'eco')
+      color = "green";
+    else if (this.props.status.thermostatMode === 'dry')
+      color = "#753500";
+
+
     const color_strip = {
       width: '100%',
       height: '20px',
       borderRadius: '20px 20px 0px 0px',
-      backgroundColor: 'yellow',
+      backgroundColor: color,
       opacity: this.props.status.on ? '1' : '0.4'
     }
 
@@ -38,9 +66,12 @@ class AcUnit extends React.Component {
           <div style={ color_strip }></div>
           <h2 style={ title }>{ this.props.device.name.name }</h2>
           <hr style={ devider }/>
-          <OnOff id={ this.props.device.id } on={ this.props.status.on } reload={ this.props.reload }/>
-          <Information id={ this.props.device.id }/>
-          <Edit id={ this.props.device.id }/>
+          <div style={traits_container}>
+            <TemperatureSetting id={ this.props.device.id } status={this.props.status} on={ this.props.status } reload={ this.props.reload }/>
+            <OnOff id={ this.props.device.id } on={ this.props.status.on } reload={ this.props.reload }/>
+            <Information id={ this.props.device.id }/>
+            <Edit id={ this.props.device.id }/>
+          </div>
         </div>
       </div>
     );
