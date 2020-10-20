89ad6e016d9b158bd1a130a9d04daa1ca9ee5820
Enrique Gomez <enrique.gomez.electronico@gmail.com>
2020-10-20 20:37:10 +0200

    the assistant is no longer needed

--- web/src/App.js
+++ web/src/App.js
@@ -4,16 +4,6 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import getCookieValue from './functions'
 import { root } from './constants'
 
-import Welcome from './components/assistant/Welcome'
-import User from './components/assistant/User'
-import Domain from './components/assistant/Domain'
-import Confignginx from './components/assistant/Confignginx'
-import Change2domain from './components/assistant/Change2domain'
-import Changed2domain from './components/assistant/Changed2domain'
-import Ssl from './components/assistant/Ssl'
-import Google from './components/assistant/Google'
-import Initialize from './components/assistant/Initialize'
-
 import Menu from './components/web/Menu'
 import Devices from './components/pages/Devices'
 import Editor from './components/pages/Editor'
@@ -89,30 +79,13 @@ class App extends React.Component {
 
   render() {
 
-    if (!this.state.session && !window.location.href.includes('assistant') && !window.location.href.includes('login'))
+    if (!this.state.session && !window.location.href.includes('login'))
       return ''
     else if (window.location.href.includes('login'))
       return (
         <div className="Clear_app">
           <Login/>
         </div>
-      );
-    else if (window.location.href.includes('assistant'))
-      return (
-        <Router>
-          <div className="Clear_app">
-            <Route exact={ true } path="/assistant/" component={ Welcome }/>
-            <Route exact={ true } path="/assistant/welcome/" component={ Welcome }/>
-            <Route exact={ true } path="/assistant/user" component={ User }/>
-            <Route exact={ true } path="/assistant/domain" component={ Domain }/>
-            <Route exact={ true } path="/assistant/confignginx" component={ Confignginx }/>
-            <Route exact={ true } path="/assistant/change2domain" component={ Change2domain }/>
-            <Route exact={ true } path="/assistant/changed2domain" component={ Changed2domain }/>
-            <Route exact={ true } path="/assistant/ssl" component={ Ssl }/>
-            <Route exact={ true } path="/assistant/google" component={ Google }/>
-            <Route exact={ true } path="/assistant/initialize" component={ Initialize }/>
-          </div>
-        </Router>
       );
     else
       return (
