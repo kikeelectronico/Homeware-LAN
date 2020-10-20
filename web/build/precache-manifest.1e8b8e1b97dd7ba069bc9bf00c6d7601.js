1631e4d50e3eeb373d97be7f5cabc56dea9aae83
Enrique Gomez <enrique.gomez.electronico@gmail.com>
2020-10-20 21:53:22 +0200

    build

---
+++ web/build/precache-manifest.1e8b8e1b97dd7ba069bc9bf00c6d7601.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77e5a7e78eb9251589dc20a6a844cfc5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9730869f5ef3786b0aae",
+    "url": "/static/css/main.3f71f348.chunk.css"
+  },
+  {
+    "revision": "6bb3137d85267586fa3b",
+    "url": "/static/js/2.7b69f0ba.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "/static/js/2.7b69f0ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9730869f5ef3786b0aae",
+    "url": "/static/js/main.6ce0cea7.chunk.js"
+  },
+  {
+    "revision": "ef13c11e300e1acbb1b7",
+    "url": "/static/js/runtime-main.db8f2875.js"
+  }
+]);
