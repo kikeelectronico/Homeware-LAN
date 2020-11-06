b388c192e463c28536d00c9baa3e13faa454d9a5
Enrique Gomez <enrique.gomez.electronico@gmail.com>
2020-11-06 20:06:29 +0100

    unit test - something is wrong

--- back/test_data.py
+++ back/test_data.py
@@ -59,7 +59,7 @@ class TestData(unittest.TestCase):
         print(self.data.getDevices())
         # Crate a new device and check if it is saved in the ddbb
         self.data.createDevice({"device":device,"status":status})
-        print(self.data.getDevices
+        print(self.data.getDevices())
         self.assertEqual(device,self.data.getDevices()[1])
         self.assertEqual(status,self.data.getStatus()[device['id']])
         # Update the device info
