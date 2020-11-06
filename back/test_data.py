304547ec60e1293b7eaba183a574a7329f9f5999
Enrique Gomez <enrique.gomez.electronico@gmail.com>
2020-11-06 19:20:03 +0100

    unit test - test data update

--- back/test_data.py
+++ back/test_data.py
@@ -62,7 +62,7 @@ class TestData(unittest.TestCase):
         self.assertEqual(status,self.data.getStatus()[device['id']])
         # Update the device info
         device['name']['name'] = 'Diodi'
-        self.data.updateDevice({"device": device})
+        self.assertTrue(self.data.updateDevice({"device": device}))
         self.assertEqual('Diodi',self.data.getDevices()[1]['name']['name'])
         # Update the device status
         self.assertFalse(self.data.getStatus()[device['id']]['on'])
