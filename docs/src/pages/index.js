7f87b24834bdaee2e92656f9b2e2790fa5c46f38
kikeelectronico <enrique.gomez.electronico@gmail.com>
2020-12-07 16:13:02 +0100

    docs - change link

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -80,7 +80,7 @@ function Home() {
                 'button button--outline button--secondary button--lg',
                 styles.getStarted,
               )}
-              to={useBaseUrl('docs/')}>
+              to={useBaseUrl('docs/intall')}>
               Get Started
             </Link>
           </div>
