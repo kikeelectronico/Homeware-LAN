02a661ff651e7d7b9da551719fe8da95fd14b7a9
kikeelecronico <enrique.gomez.electronico@gmail.com>
2021-01-24 00:06:54 +0100

    new installation method tested

--- bash/install.sh
+++ bash/install.sh
@@ -16,8 +16,8 @@ case "$response" in
         LOCATION=$(curl -s https://api.github.com/repos/kikeelectronico/Homeware-LAN/releases/latest \
           | grep "tag_name" \
           | awk '{print "https://github.com/kikeelectronico/Homeware-LAN/archive/" substr($2, 2, length($2)-3) ".zip"}') \
-          ; curl -L -o Homeware-LAN.zip $LOCATION
-        unzip Homeware-LAN.zip
+          ; sudo curl -L -o Homeware-LAN.zip $LOCATION
+        sudo unzip Homeware-LAN.zip
         sudo mv Homeware-LAN-*/ /usr/share/Homeware-LAN
         cd /usr/share
         sudo chmod -R 777 Homeware-LAN
--- bash/update.sh
+++ bash/update.sh
@@ -2,25 +2,25 @@
 
 echo "The upgrader has started.\r\n"
 
-# Delete the build to generate a 500 error
-sudo rm -r ../front/build
-
 # Download the last version
 LOCATION=$(curl -s https://api.github.com/repos/kikeelectronico/Homeware-LAN/releases/latest \
 | grep "tag_name" \
 | awk '{print "https://github.com/kikeelectronico/Homeware-LAN/archive/" substr($2, 2, length($2)-3) ".zip"}') \
-; curl -L -o Homeware-LAN.zip $LOCATION
+; sudo curl -L -o Homeware-LAN.zip $LOCATION
 
-unzip Homeware-LAN.zip
+sudo unzip Homeware-LAN.zip
 
 # Move files
-mv Homeware-LAN-*/* ../
-mv Homeware-LAN-*/back/* ../back/
-mv Homeware-LAN-*/bash/* ../bash/
-mv Homeware-LAN-*/configuration_templates/* ../configuration_templates/
-mv Homeware-LAN-*/ESP8266example/* ../ESP8266example/
-mv Homeware-LAN-*/docs/* ../docs/
-mv Homeware-LAN-*/front/* ../front/
+sudo rm -r ../configuration_templates
+sudo rm -r ../ESP8266example
+sudo rm -r ../docs
+sudo rm -r ../front
+sudo mv Homeware-LAN-*/* ../
+sudo mv Homeware-LAN-*/bash/* ../bash/
+sudo mv Homeware-LAN-*/back/* ../back/
+
+# Delete the build to generate a 500 error
+sudo rm -r ../front/build
 
 #Update Python modules
 pip3 install -r requirements.txt
@@ -34,5 +34,9 @@ sudo npm run build
 sudo systemctl restart homewareMQTT
 sudo systemctl restart homewareTasks
 sudo systemctl restart homeware
+
+cd ../back
+sudo rm -r Homeware-LAN-*
+sudo rm Homeware-LAN.zip
 
 echo "\r\The upgrader has finished.\r\n"
