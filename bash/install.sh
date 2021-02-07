b33c21937378e90f2c231da489f57e5022f1ab72
enrique.gomez <enrique.gomez@bq.com>
2021-02-07 15:44:48 +0100

    update install and update scripts

--- bash/install.sh
+++ bash/install.sh
@@ -14,11 +14,11 @@ case "$response" in
         sudo apt install unzip -y
         # Download the last version
         LOCATION=$(curl -s https://api.github.com/repos/kikeelectronico/Homeware-LAN/releases/latest \
-          | grep "tag_name" \
-          | awk '{print "https://github.com/kikeelectronico/Homeware-LAN/archive/" substr($2, 2, length($2)-3) ".zip"}') \
-          ; sudo curl -L -o Homeware-LAN.zip $LOCATION
+        | grep "tag_name" \
+        | awk '{print "https://github.com/kikeelectronico/Homeware-LAN/archive/" substr($2, 2, length($2)-3) ".zip"}') \
+        ; sudo curl -L -o Homeware-LAN.zip $LOCATION
         sudo unzip Homeware-LAN.zip
-        sudo mv Homeware-LAN-*/ /usr/share/Homeware-LAN
+        sudo mv Homeware-LAN/ /usr/share/Homeware-LAN
         cd /usr/share
         sudo chmod -R 777 Homeware-LAN
         cd Homeware-LAN
