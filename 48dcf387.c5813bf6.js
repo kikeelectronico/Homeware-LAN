cbc68055b4972a54ca78adfef689a770249b7f99
gh-actions <actions@gihub.com>
2021-10-21 20:39:09 +0000

    Deploy website - based on e57293f04f812fc03bc5f9332cb07e8933a866d4

--- 48dcf387.c5813bf6.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[11],{78:function(e,t,a){"use strict";a.r(t);var n=a(0),i=a.n(n),r=a(104);a(143);t.default=function(){return i.a.createElement(r.a,{title:"API"},i.a.createElement("iframe",{src:"/Homeware-LAN/api-build/",style:{width:"100%",height:800}}))}}}]);
---
+++ a5b8d3b3.01a469ee.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[21],{89:function(e){e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Hola","permalink":"/Homeware-LAN/blog/hola"}]}')}}]);
