f19d8d0f648de573d866626b16eb64db9735f3ee
enrique.gomez <enrique.gomez@bq.com>
2022-02-14 22:42:19 +0100

    test 3

--- back/test_data.py
+++ back/test_data.py
@@ -106,46 +106,46 @@ class Test_data(unittest.TestCase):
 				name = device['name']['name']
 		self.assertEqual('Diodi',name)
 
-	# def test_updateStatus(self):
-	# 	device = {
-	# 		"attributes": {
-	# 		  "commandOnlyOnOff": True,
-	# 		  "queryOnlyOnOff": True,
-	# 		  "commandOnlyBrightness": True
-	# 		},
-	# 		"deviceInfo": {
-	# 		  "hwVersion": "1.0",
-	# 		  "swVersion": "1.0",
-	# 		  "manufacturer": "Homeware",
-	# 		  "model": "Homeware Lamp 2.0"
-	# 		},
-	# 		"id": "light003",
-	# 		"name": {
-	# 		  "defaultNames": [
-	# 			"Lamp"
-	# 		  ],
-	# 		  "nicknames": [
-	# 			"Lamp"
-	# 		  ],
-	# 		  "name": "Test Lamp"
-	# 		},
-	# 		"traits": [
-	# 		  "action.devices.traits.OnOff",
-	# 		  "action.devices.traits.Brightness"
-	# 		],
-	# 		"type": "action.devices.types.LIGHT"
-	# 	}
-	# 	status = {
-	# 		"online": True,
-	# 		"on": False,
-	# 		"brightness": 80
-	# 	}
-	# 	# Crate a new device
-	# 	self.data.createDevice({"device":device,"status":status})
-	# 	# Update the device status
-	# 	self.assertFalse(self.data.getStatus()['light003']['on'])
-	# 	self.data.updateParamStatus('light003',"on",True)
-	# 	self.assertTrue(self.data.getStatus()['light003']['on'])
+	def test_updateStatus(self):
+		device = {
+			"attributes": {
+			  "commandOnlyOnOff": True,
+			  "queryOnlyOnOff": True,
+			  "commandOnlyBrightness": True
+			},
+			"deviceInfo": {
+			  "hwVersion": "1.0",
+			  "swVersion": "1.0",
+			  "manufacturer": "Homeware",
+			  "model": "Homeware Lamp 2.0"
+			},
+			"id": "light003",
+			"name": {
+			  "defaultNames": [
+				"Lamp"
+			  ],
+			  "nicknames": [
+				"Lamp"
+			  ],
+			  "name": "Test Lamp"
+			},
+			"traits": [
+			  "action.devices.traits.OnOff",
+			  "action.devices.traits.Brightness"
+			],
+			"type": "action.devices.types.LIGHT"
+		}
+		status = {
+			"online": True,
+			"on": False,
+			"brightness": 80
+		}
+		# Crate a new device
+		self.data.createDevice({"device":device,"status":status})
+		# Update the device status
+		self.assertFalse(self.data.getStatus()['light003']['on'])
+		self.data.updateParamStatus('light003',"on",True)
+		self.assertTrue(self.data.getStatus()['light003']['on'])
 
 	# def test_deleteDevice(self):
 	# 	device = {
