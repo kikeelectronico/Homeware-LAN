a5c6c51f8f9f9e9db93ded23b351daf041949fb0
enrique.gomez <enrique.gomez@bq.com>
2022-02-14 22:33:03 +0100

    test version

--- back/test_data.py
+++ back/test_data.py
@@ -12,8 +12,8 @@ class Test_data(unittest.TestCase):
 
 # GLOBAL
 
-	# def test_version(self):
-	# 	self.assertEqual('v', self.data.getVersion()['version'][0])
+	def test_version(self):
+		self.assertEqual('v', self.data.getVersion()['version'][0])
 
 	# def test_redisStatus(self):
 	# 	response = {
