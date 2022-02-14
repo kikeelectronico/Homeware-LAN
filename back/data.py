487daa6865b1862e9186e62995e3b54101e0974b
enrique.gomez <enrique.gomez@bq.com>
2022-02-14 22:18:07 +0100

    verbose

--- back/data.py
+++ back/data.py
@@ -32,7 +32,7 @@ class Data:
 	def __init__(self):
 		print("test here")
 		# self.redis = redis.Redis(hostname.REDIS_HOST, hostname.REDIS_PORT)
-		# self.verbose = False
+		self.verbose = False
 
 		# if not self.redis.get('transfer'):
 		# 	self.log('Warning','The database must be created')
