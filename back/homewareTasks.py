83959fcd258449b4b0e50fcf412bb2a6291de9e1
enrique.gomez <enrique.gomez@bq.com>
2022-07-20 20:52:26 +0200

    verify ip format

--- back/homewareTasks.py
+++ back/homewareTasks.py
@@ -1,3 +1,4 @@
+from ipaddress import ip_address
 import time
 from datetime import datetime
 import json
@@ -33,62 +34,67 @@ def verifyTasks():
 def ddnsUpdater():
 	ddns = data_conector.getDDNS()
 	ipServer = 'http://ip1.dynupdate.no-ip.com/'
-	if ddns['enabled']:
+	ip_bytes = ipServer.split(".")
+	if len(ip_bytes) == 4 \
+		and int(ip_bytes[0]) > 0 and int(ip_bytes[0]) < 255 \
+		and int(ip_bytes[1]) > 0 and int(ip_bytes[1]) < 255 \
+		and int(ip_bytes[2]) > 0 and int(ip_bytes[2]) < 255 \
+		and int(ip_bytes[3]) > 0 and int(ip_bytes[3]) < 255:
+		if ddns['enabled']:
+			ipRequest = requests.get(url=ipServer)
+			newIP = ipRequest.text
+			if not newIP == ddns['ip']:
 
-		ipRequest = requests.get(url=ipServer)
-		newIP = ipRequest.text
-		if not newIP == ddns['ip']:
-
-			if ddns['provider'] == 'noip':
-				noipServer = 'https://dynupdate.no-ip.com/nic/update'
-				params = {
-					'hostname': ddns['hostname'],
-					'myip': newIP
-				}
-				user = ddns['username'] + ':' + ddns['password']
-				userEncoded = str(b64encode(bytes(user, 'utf-8')))
-				headers = {
-					'User-Agent': 'Homeware Homeware/v{} hola@rinconingenieril.es'.format(data_conector.getVersion()['version']),
-					'Authorization': 'Basic ' + userEncoded[2:len(userEncoded)-1]
-				}
-				noipRequest = requests.get(url= noipServer, params=params, headers=headers)
-				#Analyze the response
-				code = noipRequest.text.split(' ')[0]
-				data_conector.log('Log', 'IP update request sended to no-ip. Response: ' + noipRequest.text)
-				status = {
-					'good': 'Running',
-					'nochg': 'Running, but the last request shouldn\'t have been done.',
-					'nohost': 'Host name does not exists',
-					'badauth': 'Invalid username and/or password',
-					'badagent': 'Bad agent. Please open an issue on the Homeware-LAN github and do not enable the DDNS funtionality.',
-					'!donator': 'Not donator. Please open an issue on the Homeware-LAN github and do not enable the DDNS funtionality.',
-					'abuse': 'User blocked due to abuse',
-					'911': 'Something goes wrong. Do not enabled until 30 minutes has pass.'
-				}
-				now = datetime.now()
-				last = str(now.strftime("%m/%d/%Y, %H:%M:%S"))
-				if not 'good' in code and not 'nochg' in code:
-					code = noipRequest.text.split('\r')[0]
-					data_conector.updateDDNS(newIP, status[code], code, False, last)
-				else:
-					data_conector.updateDDNS(newIP, status[code], code, True, last)
-			elif ddns['provider'] == 'duckdns':
-				duckdnsServer = 'https://www.duckdns.org/update'
-				params = {
-					'domains': ddns['hostname'].split('.')[0],
-					'ip': newIP,
-					'token': ddns['password']
-				}
-				duckDnsRequest = requests.get(url= duckdnsServer, params=params)
-				#Analyze the response
-				code = duckDnsRequest.text
-				data_conector.log('Log', 'IP update request sended to Duck DNS. Response: ' + code)
-				now = datetime.now()
-				last = str(now.strftime("%m/%d/%Y, %H:%M:%S"))
-				if "OK" in code:
-					data_conector.updateDDNS(newIP, code, code, True, last)
-				else:
-					data_conector.updateDDNS(newIP, code, code, False, last)
+				if ddns['provider'] == 'noip':
+					noipServer = 'https://dynupdate.no-ip.com/nic/update'
+					params = {
+						'hostname': ddns['hostname'],
+						'myip': newIP
+					}
+					user = ddns['username'] + ':' + ddns['password']
+					userEncoded = str(b64encode(bytes(user, 'utf-8')))
+					headers = {
+						'User-Agent': 'Homeware Homeware/v{} hola@rinconingenieril.es'.format(data_conector.getVersion()['version']),
+						'Authorization': 'Basic ' + userEncoded[2:len(userEncoded)-1]
+					}
+					noipRequest = requests.get(url= noipServer, params=params, headers=headers)
+					#Analyze the response
+					code = noipRequest.text.split(' ')[0]
+					data_conector.log('Log', 'IP update request sended to no-ip. Response: ' + noipRequest.text)
+					status = {
+						'good': 'Running',
+						'nochg': 'Running, but the last request shouldn\'t have been done.',
+						'nohost': 'Host name does not exists',
+						'badauth': 'Invalid username and/or password',
+						'badagent': 'Bad agent. Please open an issue on the Homeware-LAN github and do not enable the DDNS funtionality.',
+						'!donator': 'Not donator. Please open an issue on the Homeware-LAN github and do not enable the DDNS funtionality.',
+						'abuse': 'User blocked due to abuse',
+						'911': 'Something goes wrong. Do not enabled until 30 minutes has pass.'
+					}
+					now = datetime.now()
+					last = str(now.strftime("%m/%d/%Y, %H:%M:%S"))
+					if not 'good' in code and not 'nochg' in code:
+						code = noipRequest.text.split('\r')[0]
+						data_conector.updateDDNS(newIP, status[code], code, False, last)
+					else:
+						data_conector.updateDDNS(newIP, status[code], code, True, last)
+				elif ddns['provider'] == 'duckdns':
+					duckdnsServer = 'https://www.duckdns.org/update'
+					params = {
+						'domains': ddns['hostname'].split('.')[0],
+						'ip': newIP,
+						'token': ddns['password']
+					}
+					duckDnsRequest = requests.get(url= duckdnsServer, params=params)
+					#Analyze the response
+					code = duckDnsRequest.text
+					data_conector.log('Log', 'IP update request sended to Duck DNS. Response: ' + code)
+					now = datetime.now()
+					last = str(now.strftime("%m/%d/%Y, %H:%M:%S"))
+					if "OK" in code:
+						data_conector.updateDDNS(newIP, code, code, True, last)
+					else:
+						data_conector.updateDDNS(newIP, code, code, False, last)
 
 def operationExecutor(id, triggers, status):
 	operation = triggers[str(id)]
