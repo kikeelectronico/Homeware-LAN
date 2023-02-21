c9366f483a7cd46906361cc1b560936bd563951a
enrique.gomez <enrique.gomez@bq.com>
2023-02-21 22:50:33 +0100

    add a post about how to update Homeware

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -2,6 +2,7 @@ module.exports = {
   someSidebar: {
     Installation: ["install", "connect-with-google", "google-auth"],
     Guides: ["create-device", "connecting-hardware"],
+    Maintenance: ["update-homeware"],
     "4 Homeware devs": ["directories", "datapy", "commandspy", "homegraphpy", "homewarepy", "homewaremqttpy", "homewaretaskpy"], 
   },
 };
