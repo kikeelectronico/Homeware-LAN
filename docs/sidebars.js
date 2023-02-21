8f40e706bc40022d93361faa1c105ee1745e86cd
enrique.gomez <enrique.gomez@bq.com>
2023-02-21 22:50:17 +0100

    delete the help post about renew the certificate

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -2,7 +2,6 @@ module.exports = {
   someSidebar: {
     Installation: ["install", "connect-with-google", "google-auth"],
     Guides: ["create-device", "connecting-hardware"],
-    Maintenance: ["renew-ssl-certificate"],
     "4 Homeware devs": ["directories", "datapy", "commandspy", "homegraphpy", "homewarepy", "homewaremqttpy", "homewaretaskpy"], 
   },
 };
