7fcf044329d62190d8071b09be3b7c42f3a7e188
Enrique <enrique.gomez.electronico@gmail.com>
2023-08-27 14:53:06 +0200

    refactor param controls

--- front/src/components/devices/traits/Brightness.js
+++ front/src/components/devices/traits/Brightness.js
@@ -1,28 +1,13 @@
 import React from 'react';
 
-class Brightness extends React.Component {
-
-  render() {
-
-    const container = {
-      float: 'left',
-      marginLeft: '5px',
-      height: "30px",
-    }
-
-    const title = {
-      color: "#777",
-      fontSize: 25,
-      lineHeight: "30px",
-      verticalAlign: "middle",
-    };
-
-    return (
-      <div style={container}>
-        <span style={title}>{this.props.brightness} %</span>
-      </div>
-    );
-  }
+const Brightness = (props) => {
+  
+  return (
+    <div style={{float: 'left', marginLeft: '5px', height: "30px"}}>
+      <span style={{color: "#777", fontSize: 25, lineHeight: "30px", verticalAlign: "middle"}}>{props.brightness} %</span>
+    </div>
+  );
+  
 }
 
 export default Brightness
--- front/src/components/devices/traits/TemperatureSetting.js
+++ front/src/components/devices/traits/TemperatureSetting.js
@@ -2,89 +2,51 @@ import React from "react";
 import getCookieValue from "../../../functions";
 import { root } from "../../../constants";
 
-class TemperatureSetting extends React.Component {
-  constructor(props) {
-    super(props);
-    this.up = this.up.bind(this);
-    this.down = this.down.bind(this);
-    this.update = this.update.bind(this);
+const TemperatureSetting = (props) => {
+
+  const up = () => {
+    var temperature = props.status.thermostatTemperatureSetpoint + 1;
+    update(temperature);
   }
 
-  up() {
-    var temperature = this.props.status.thermostatTemperatureSetpoint + 1;
-    this.update(temperature);
+  const down = () => {
+    var temperature = props.status.thermostatTemperatureSetpoint - 1;
+    update(temperature);
   }
 
-  down() {
-    var temperature = this.props.status.thermostatTemperatureSetpoint - 1;
-    this.update(temperature);
-  }
-
-  update(temperature) {
+  const update = (temperature) => {
     var http = new XMLHttpRequest();
     http.onload = function (e) {
       if (http.readyState === 4) {
         if (http.status === 200) {
-          this.props.reload();
+          props.reload();
         } else {
           console.error(http.statusText);
         }
       }
-    }.bind(this);
+    }
     http.open("POST", root + "api/status/update/");
     http.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
     http.setRequestHeader("authorization", "baerer " + getCookieValue("token"));
     http.send(
       JSON.stringify({
-        id: this.props.id,
+        id: props.id,
         param: "thermostatTemperatureSetpoint",
         value: temperature,
       })
     );
   }
 
-  render() {
-    const container = {
-      float: "left",
-      marginLeft: "5px",
-    };
-
-    const title = {
-      color: "#777",
-    };
-
-    const image = {
-      width: "30px",
-    };
-
-    const temperature = {
-      fontSize: "30px",
-      marginLeft: "5px",
-      marginRight: "5px",
-      color: "#777",
-    };
-
-    return (
-      <div style={container}>
-        <span style={title}>to</span>
-        <img
-          src="/devices/arrow_down.png"
-          onClick={this.down}
-          alt="Arrow up"
-          style={image}
-        />
-        <span style={temperature}>
-          {this.props.status.thermostatTemperatureSetpoint}
-        </span>
-        <img
-          src="/devices/arrow_up.png"
-          onClick={this.up}
-          alt="Arrow down"
-          style={image}
-        />
-      </div>
-    );
-  }
+  return (
+    <div style={{float: 'left', marginLeft: '5px', height: "30px"}}>
+      <span style={{color: "#777"}}>to</span>
+      <img src="/devices/arrow_down.png" onClick={down} alt="Arrow up" style={{ width: "30px"}}/>
+      <span style={{fontSize: "30px", marginLeft: "5px", marginRight: "5px", color: "#777"}}>
+        {props.status.thermostatTemperatureSetpoint}
+      </span>
+      <img src="/devices/arrow_up.png" onClick={up} alt="Arrow down" style={{ width: "30px"}}/>
+    </div>
+  );
 }
 
 export default TemperatureSetting;
