7cb29c1d8a5ef1ef862be47539508e592a8720b6
Enrique <enrique.gomez.electronico@gmail.com>
2024-07-25 21:38:21 +0200

    fan speed

--- front/src/components/editor/traits/FanSpeed.js
+++ front/src/components/editor/traits/FanSpeed.js
@@ -1,129 +1,223 @@
-import React from 'react';
+import React, {useEffect, forwardRef, useImperativeHandle, useState} from 'react';
 import Switch from "react-switch";
+import {InputLabel, Button, Select, MenuItem, Box, TextField, FormControl, IconButton, Stack} from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
 
-class FanSpeed extends React.Component {
-  constructor(props) {
-    super(props);
-    this.updateCheckbox = this.updateCheckbox.bind(this);
-    this.updateFanSpeeds = this.updateFanSpeeds.bind(this);
-    this.addFanSpeed = this.addFanSpeed.bind(this);
-  }
-
-
-  updateCheckbox(checked, attribute){
-    this.props.update('attributes/' + attribute,checked);
-  }
-
-  updateFanSpeeds(event){
-    const id = event.target.id.split('_')
-    const speed_id = id[1]
-    //Process the attribute and value depending of the attribute
-    const speed_attribute = id[0] === 'lang' ? id[0] : 'speed_synonym';
-    const value = id[0] === 'lang' ? event.target.value : event.target.value.split(',')
-    //Update the temporal data and update it
-    var temp_availableFanSpeeds = this.props.attributes.availableFanSpeeds
-    temp_availableFanSpeeds.speeds[speed_id].speed_values[0][speed_attribute] = value
-    if (id[0] === 'names') temp_availableFanSpeeds.speeds[speed_id].speed_name = value[0]
-    this.props.update('attributes/availableFanSpeeds', temp_availableFanSpeeds);
-  }
-
-  addFanSpeed(){
-    var temp_availableFanSpeeds = this.props.attributes.availableFanSpeeds
-    temp_availableFanSpeeds.speeds.push({
-      "speed_name": "",
-      "speed_values": [
-        {
-          "speed_synonym": [""],
-          "lang": "en"
-        }
-      ]
-    });
-    this.props.update('attributes/availableFanSpeeds', temp_availableFanSpeeds);
-  }
-
-  render() {
-
-    const names_box = {
-      width: '150px'
-    }
-
-    const speeds = this.props.attributes.availableFanSpeeds.speeds.map((speed, i) => {
-      return (
-              <div key={i}>
-                <div className="two_table_row" key={i}>
-                  <div className="two_table_cel">
-                  </div>
-                  <div className="two_table_cel">
-                    <label>
-                      <span>Languaje: </span>
-                      <select name="type" id={"lang_" + i} value={speed.speed_values[0].lang} onChange={this.updateFanSpeeds}>
-                        <option value="es">es</option>
-                        <option value="en">en</option>
-                      </select>
-                    </label>
-                    <label>
-                      <span>Speed name: </span>
-                      <input type="text" id={"names_" + i} style={names_box} defaultValue={speed.speed_values[0].speed_synonym} placeholder="Speed name" onChange={this.updateFanSpeeds}/>
-                    </label>
-                  </div>
-                </div>
-              </div>
-              )
-    });
-
-    return (
-      <div>
-
-        <div className="three_table_row">
-          <div className="three_table_cel align_right">
-            <i>reversible</i>
-          </div>
-          <div className="three_table_cel">
-            <Switch onChange={(checked) => {this.updateCheckbox(checked,"reversible")}} checked={this.props.attributes.reversible} />
-          </div>
-          <div className="three_table_cel">
-            <span className="attribute_advise">Enable it if the fan supports blowing in both directions.</span>
-          </div>
-        </div>
-
-        <div className="three_table_row">
-          <div className="three_table_cel align_right">
-            <i>supportsFanSpeedPercent</i>
-          </div>
-          <div className="three_table_cel">
-            <Switch onChange={(checked) => {this.updateCheckbox(checked,"supportsFanSpeedPercent")}} checked={this.props.attributes.supportsFanSpeedPercent} />
-          </div>
-          <div className="three_table_cel">
-            <span className="attribute_advise">Enable it if the speed can be controlled with a number from 0 to 100.</span>
-          </div>
-        </div>
-
-        <div className="three_table_row">
-          <div className="three_table_cel align_right">
-            <i>commandOnlyFanSpeed</i>
-          </div>
-          <div className="three_table_cel">
-            <Switch onChange={(checked) => {this.updateCheckbox(checked,"commandOnlyFanSpeed")}} checked={this.props.attributes.commandOnlyFanSpeed} />
-          </div>
-          <div className="three_table_cel">
-            <span className="attribute_advise">Enable it if Homeware-LAN shouldn't inform Google Home about the fan speed.</span>
-          </div>
-        </div>
-
-        <div className="three_table_row">
-          <div className="three_table_cel align_right">
-            Add a speed
-          </div>
-          <div className="three_table_cel">
-            <button type="button" className="add_attribute_button" onClick={ this.addFanSpeed }>Add</button>
-          </div>
-        </div>
-
-        {speeds}
-
-      </div>
-    );
-  }
+const attributes = {
+  reversible: false,
+  commandOnlyFanSpeed: false,
+  supportsFanSpeedPercent: true,
 }
+
+const states = {
+  currentFanSpeedPercent: 0
+}
+
+const speed_template = {
+  "speed_name": "sdf",
+  "speed_values": [
+    {
+      "speed_synonym": [""],
+      "lang": "en"
+    }
+  ]
+}
+
+const FanSpeed = forwardRef((props, ref) => {
+
+  const [reversible, setReversible] = useState(false)
+  const [commandOnlyFanSpeed, setCommandOnlyFanSpeed] = useState(false)
+  const [supportsFanSpeedPercent, setSupportsFanSpeedPercent] = useState(true)
+  const [availableFanSpeeds, setAvailableFanSpeeds] = useState({speeds: [], ordered: false})
+
+  useEffect(() => {
+    if ("reversible" in props.attributes) {
+      setReversible(props.attributes.reversible)
+      setCommandOnlyFanSpeed(props.attributes.commandOnlyFanSpeed)
+      if ("supportsFanSpeedPercent" in props.attributes)
+        setSupportsFanSpeedPercent(props.attributes.supportsFanSpeedPercent)      
+      if ("availableFanSpeeds" in props.attributes)
+        setAvailableFanSpeeds(props.attributes.availableFanSpeeds)
+    } else {
+      props.updateStatus(null, states, "insert")
+      props.updateAttributes(null, attributes, "insert")
+    }
+  }, [])
+
+  useImperativeHandle(ref, () => ({
+    deleteAttributes() {
+      props.updateStatus(null, states, "drop")
+      props.updateAttributes(null, attributes, "drop")
+    }
+  }))
+
+  useEffect(() => {
+    if (!supportsFanSpeedPercent) {
+      props.updateAttributes("supportsFanSpeedPercent", "", "delete")
+      props.updateAttributes("availableFanSpeeds", availableFanSpeeds, "update")
+    } else {
+      props.updateAttributes("supportsFanSpeedPercent", supportsFanSpeedPercent, "update")
+      props.updateAttributes("availableFanSpeeds", "", "delete")
+    }
+      
+      
+  }, [supportsFanSpeedPercent])
+
+  const addFanSpeed = () => {
+    let _availableFanSpeeds = {...availableFanSpeeds}
+    _availableFanSpeeds.speeds.push({...speed_template})
+    console.log(_availableFanSpeeds)
+    setAvailableFanSpeeds(_availableFanSpeeds)
+    props.updateAttributes("availableFanSpeeds", _availableFanSpeeds, "update")
+  }
+
+  const removeFanSpeed = (index) => {
+    let _availableFanSpeeds = {...availableFanSpeeds}
+    _availableFanSpeeds.speeds.splice(index, 1)
+    setAvailableFanSpeeds(_availableFanSpeeds)
+    props.updateAttributes("availableFanSpeeds", _availableFanSpeeds, "update")
+  }
+
+  const updateFanSpeed = (index, name, value) => {
+    let _availableFanSpeeds = {...availableFanSpeeds}
+    if (name === "speed_name") {
+      _availableFanSpeeds["speeds"][index]["speed_name"] = value
+      _availableFanSpeeds["speeds"][index]["speed_values"][0]["speed_synonym"] = [value]
+    }  else if (name === "lang") {
+      _availableFanSpeeds["speeds"][index]["speed_values"][0]["lang"] = value
+    }
+    console.log(index, _availableFanSpeeds)
+    setAvailableFanSpeeds(_availableFanSpeeds)
+    props.updateAttributes("availableFanSpeeds", _availableFanSpeeds, "update")
+  }
+
+  const Speed = (props) => {
+    return (
+      <Box className="attribute_table_subattribute">
+          <Box className="attribute_table_subattribute_row">
+              <FormControl fullWidth>
+                  <InputLabel id="occupancySensorType-label">
+                      Languaje
+                  </InputLabel>
+                  <Select
+                      id="lang"
+                      data-test="lang"
+                      label="Languaje"
+                      className="attribute_table_subattribute_input"
+                      value={props.speed.speed_values[0].lang}
+                      onChange={(event) => {
+                          props.updateSpeedParam(props.index, "lang", event.target.value)
+                      }}
+                  >
+                      <MenuItem value="en">en</MenuItem>
+                      <MenuItem value="es">es</MenuItem>
+                  </Select>
+              </FormControl>
+          </Box>
+          <Box className="attribute_table_subattribute_row">
+              <TextField
+                  data-test="speed_name"
+                  label="Speed name"
+                  className="attribute_table_subattribute_input"
+                  type="text"
+                  variant="outlined"
+                  value={props.speed.speed_name}
+                  onChange={(event) => {
+                      props.updateSpeedParam(props.index, "speed_name", event.target.value)
+                  }}
+              />
+          </Box>
+          <Stack direction="row" spacing={1}>
+              <IconButton size="large" onClick={() => props.removeSpeed(props.index)}>
+                  <DeleteIcon />
+              </IconButton>
+          </Stack>
+      </Box>
+  );
+  }
+
+  return (
+    <>
+      <div className="three_table_row">
+        <div className="three_table_cel align_right">
+          <i>reversible</i>
+        </div>
+        <div className="three_table_cel">
+          <Switch
+            onChange={(checked) => {
+              setReversible(checked)
+              props.updateAttributes("reversible", checked, "update")
+            }}
+            checked={reversible}
+          />
+        </div>
+      </div>
+
+      <div className="three_table_row">
+        <div className="three_table_cel align_right">
+          <i>commandOnlyFanSpeed</i>
+        </div>
+        <div className="three_table_cel">
+          <Switch
+            onChange={(checked) => {
+              setCommandOnlyFanSpeed(checked)
+              props.updateAttributes("commandOnlyFanSpeed", checked, "update")
+            }}
+            checked={commandOnlyFanSpeed}
+          />
+        </div>
+      </div>
+
+      <div className="three_table_row">
+        <div className="three_table_cel align_right">
+          <i>supportsFanSpeedPercent</i>
+        </div>
+        <div className="three_table_cel">
+          <Switch
+            onChange={(checked) => {
+              setSupportsFanSpeedPercent(checked)
+              props.updateAttributes("supportsFanSpeedPercent", checked, "update")
+            }}
+            checked={supportsFanSpeedPercent}
+          />
+        </div>
+      </div>
+
+      {
+        supportsFanSpeedPercent ? <></> :
+
+        <div className="three_table_row">
+          <div className="three_table_cel align_right">
+            <i>availableFanSpeeds</i>
+          </div>
+          <div className="three_table_cel">
+            {
+                availableFanSpeeds.speeds.map((speed, index) => {
+                    return <Speed
+                                speed={speed}
+                                index={index}
+                                removeSpeed={removeFanSpeed}
+                                updateSpeedParam={updateFanSpeed}
+                                key={index}
+                            />
+                })
+            }
+            <Box className="attribute_table_form_add_button">
+                <Button
+                    variant="contained"
+                    className="attribute_table_form_add_button"
+                    onClick={addFanSpeed}
+                >
+                    Add
+                </Button>
+            </Box>
+          </div>
+        </div>
+      }
+
+    </>
+  );
+
+})
 
 export default FanSpeed
