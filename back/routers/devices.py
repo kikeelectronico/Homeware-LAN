f365be8185a63f67cc635cc74a61920252b0a4ec
Enrique <enrique.gomez.electronico@gmail.com>
2024-08-25 21:20:56 +0200

    add all devices endpoints

--- back/routers/devices.py
+++ back/routers/devices.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, Depends, HTTPException, status, Header
+from fastapi import APIRouter, Depends
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 from typing import Annotated
 
@@ -27,3 +28,49 @@ def getDevices(device_id: str | None):
     else:
         return data_conector.getDevices()
     
+class Device(BaseModel):
+    device: dict
+    status: dict
+
+@router.post("/api/devices/update/", dependencies=[Depends(allowAuthenticated)])
+@router.post("/api/devices/update/", dependencies=[Depends(allowAuthenticated)])
+def updateDevices(device: Device):
+    if device:
+        if data_conector.updateDevice(device.device, device.status):
+            return JSONResponse(status_code=200,
+                                content = {
+                                    "error": "Success",
+                                    "code": 200,
+                                })
+        else:
+            return errorResponses.FOUR_O_FOUR
+    else:
+        return errorResponses.FOUR_O_O
+
+@router.post("/api/devices/create/", dependencies=[Depends(allowAuthenticated)])
+@router.post("/api/devices/create/", dependencies=[Depends(allowAuthenticated)])
+def createDevices(device: Device):
+    if device:
+        data_conector.createDevice(device.device, device.status)
+        return JSONResponse(status_code=200,
+                            content = {
+                                "error": "Success",
+                                "code": 200,
+                            })
+    else:
+        return errorResponses.FOUR_O_O
+
+@router.post("/api/devices/delete/{device_id}", dependencies=[Depends(allowAuthenticated)])
+@router.post("/api/devices/delete/{device_id}/", dependencies=[Depends(allowAuthenticated)])
+def createDevices(device_id: str | None):
+     if device_id:
+        if data_conector.deleteDevice(device_id):
+            return JSONResponse(status_code=200,
+                                content = {
+                                    "error": "Success",
+                                    "code": 200,
+                                })
+        else:
+            return errorResponses.FOUR_O_FOUR
+    else:
+        return errorResponses.FOUR_O_O
