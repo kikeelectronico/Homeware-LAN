703d1f2ef4ab43abdc9c5d477873630730e715d1
Enrique <enrique.gomez.electronico@gmail.com>
2024-08-25 20:51:50 +0200

    add get devices

--- back/routers/devices.py
+++ back/routers/devices.py
@@ -4,6 +4,7 @@ from typing import Annotated
 
 from security.authentication import allowAuthenticated
 from data import Data
+import errorResponses
 
 router = APIRouter()
 data_conector = Data()
@@ -13,3 +14,16 @@ data_conector = Data()
 def getDevices():
     response = data_conector.getGlobal()
     return response
+
+@router.get("/api/devices/get", dependencies=[Depends(allowAuthenticated)])
+@router.get("/api/devices/get/", dependencies=[Depends(allowAuthenticated)])
+@router.get("/api/devices/get/{device_id}", dependencies=[Depends(allowAuthenticated)])
+@router.get("/api/devices/get/{device_id}/", dependencies=[Depends(allowAuthenticated)])
+def getDevices(device_id: str | None):
+    if device_id:
+        response = data_conector.getDevices(device_id)
+        if not response: return errorResponses.FOUR_O_FOUR
+        return response
+    else:
+        return data_conector.getDevices()
+    
