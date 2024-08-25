6b4e0e46ac67043fdaaee544a0612c9a264e1b36
Enrique <enrique.gomez.electronico@gmail.com>
2024-08-25 21:48:20 +0200

    users endpoints

--- back/routers/users.py
+++ back/routers/users.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 from typing import Annotated
 
 from data import Data
+import errorResponses
 
 router = APIRouter()
 data_conector = Data()
@@ -10,18 +11,23 @@ data_conector = Data()
 @router.get("/api/user/validateToken")
 @router.get("/api/user/validateToken/")
 def validateUserToken(token: Annotated[str | None, Header()] = None):
-    response = {
-        'status': 'in' if data_conector.validateUserToken(token) else "fail"
-    }
-    return response
+    if token:
+        return {
+            'status': 'in' if data_conector.validateUserToken(token) else "fail"
+        }
+    else:
+        return errorResponses.FOUR_O_O
 
 @router.get("/api/user/login")
 @router.get("/api/user/login/")
 def login(username: Annotated[str | None, Header()] = None,
             password: Annotated[str | None, Header()] = None):
-    token = data_conector.login(username, password)
-    return {
-        'status': 'in' if token is not None else "fail",
-        'user': username,
-        'token': token if token is not None else ""
-    }
+    if username and password:
+        token = data_conector.login(username, password)
+        return {
+            'status': 'in' if token is not None else "fail",
+            'user': username,
+            'token': token if token is not None else ""
+        }
+    else:
+        return errorResponses.FOUR_O_O
