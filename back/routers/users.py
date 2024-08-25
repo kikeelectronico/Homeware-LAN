e56fb89602187bbca0f3833db0fcbf14c8cac491
Enrique <enrique.gomez.electronico@gmail.com>
2024-08-25 21:11:58 +0200

    add login

--- back/routers/users.py
+++ back/routers/users.py
@@ -14,3 +14,14 @@ def validateUserToken(token: Annotated[str | None, Header()] = None):
         'status': 'in' if data_conector.validateUserToken(token) else "fail"
     }
     return response
+
+@router.get("/api/user/login")
+@router.get("/api/user/login/")
+def login(username: Annotated[str | None, Header()] = None,
+            password: Annotated[str | None, Header()] = None):
+    token = data_conector.login(username, password)
+    return {
+        'status': 'in' if token is not None else "fail",
+        'user': username,
+        'token': token if token is not None else ""
+    }
