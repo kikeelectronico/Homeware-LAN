18ccb428dc65e709b96e13c729a9a680e2b41135
Enrique <enrique.gomez.electronico@gmail.com>
2024-08-25 21:54:15 +0200

    add 403 and fix codes

--- back/errorResponses.py
+++ back/errorResponses.py
@@ -1,9 +1,16 @@
 from fastapi.responses import JSONResponse
 
-FOUR_O_ONE = JSONResponse(status_code=404,
+FOUR_O_ONE = JSONResponse(status_code=401,
                             content = {
-                                "error": "Bad authentication",
+                                "error": "Authentication is needed",
                                 "code": 401,
+                                "note": "See the documentation https://homeware.enriquegomez.me/api-docs.html"
+                            })
+
+FOUR_O_THREE = JSONResponse(status_code=403,
+                            content = {
+                                "error": "Authentication fail",
+                                "code": 403,
                                 "note": "See the documentation https://homeware.enriquegomez.me/api-docs.html"
                             })
 
@@ -14,7 +21,7 @@ FOUR_O_FOUR = JSONResponse(status_code=404,
                                 "note": "See the documentation https://homeware.enriquegomez.me/"
                             })
                             
-FOUR_O_O = JSONResponse(status_code=404,
+FOUR_O_O = JSONResponse(status_code=400,
                             content = {
                                 "error": "Operation not supported",
                                 "code": 400,
