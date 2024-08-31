03dd40a610257970c92acc494b7bc36650a0f034
Enrique <enrique.gomez.electronico@gmail.com>
2024-08-31 19:04:56 +0200

    log end points tests

---
+++ back/tests_integration/test_api_log.py
@@ -0,0 +1,122 @@
+import requests
+import os
+import json
+import pytest
+
+# /api/log/*
+
+def test_get_log():
+    headers = {
+        "authorization": f"baerer {pytest.token}",
+        "content-type": "application/json"
+    }
+    request = requests.get(pytest.host + "/api/log/get", headers=headers)
+    assert request.status_code == 200
+    response = request.json()
+    assert isinstance(response, list)
+
+def test_get_log_fail_bad_token():
+    headers = {
+        "authorization": f"baerer whre_is_perry",
+        "content-type": "application/json"
+    }
+    request = requests.get(pytest.host + "/api/log/get", headers=headers)
+    assert request.status_code == 401
+    response = request.json()
+    assert response["error"] == "Bad authentication"
+    assert response["code"] == 401
+    assert response["note"] == "See the documentation https://homeware.enriquegomez.me/api-docs.html"
+
+def test_get_log_fail_no_token_value():
+    headers = {
+        "authorization": f"baerer ",
+        "content-type": "application/json"
+    }
+    request = requests.get(pytest.host + "/api/log/get", headers=headers)
+    assert request.status_code == 401
+    response = request.json()
+    assert response["error"] == "Bad authentication"
+    assert response["code"] == 401
+    assert response["note"] == "See the documentation https://homeware.enriquegomez.me/api-docs.html"
+
+
+def test_get_log_fail_no_header_value():
+    headers = {
+        "authorization": f"",
+        "content-type": "application/json"
+    }
+    request = requests.get(pytest.host + "/api/log/get", headers=headers)
+    assert request.status_code == 401
+    response = request.json()
+    assert response["error"] == "Bad authentication"
+    assert response["code"] == 401
+    assert response["note"] == "See the documentation https://homeware.enriquegomez.me/api-docs.html"
+
+def test_get_log_fail_no_header():
+    headers = {
+        "content-type": "application/json"
+    }
+    request = requests.get(pytest.host + "/api/log/get", headers=headers)
+    assert request.status_code == 401
+    response = request.json()
+    assert response["error"] == "Bad authentication"
+    assert response["code"] == 401
+    assert response["note"] == "See the documentation https://homeware.enriquegomez.me/api-docs.html"
+
+def test_get_alert():
+    headers = {
+        "authorization": f"baerer {pytest.token}",
+        "content-type": "application/json"
+    }
+    request = requests.get(pytest.host + "/api/log/alert", headers=headers)
+    assert request.status_code == 200
+    response = request.json()
+    assert "alert" in response
+
+def test_get_alert_fail_bad_token():
+    headers = {
+        "authorization": f"baerer whre_is_perry",
+        "content-type": "application/json"
+    }
+    request = requests.get(pytest.host + "/api/log/alert", headers=headers)
+    assert request.status_code == 401
+    response = request.json()
+    assert response["error"] == "Bad authentication"
+    assert response["code"] == 401
+    assert response["note"] == "See the documentation https://homeware.enriquegomez.me/api-docs.html"
+
+def test_get_alert_fail_no_token_value():
+    headers = {
+        "authorization": f"baerer ",
+        "content-type": "application/json"
+    }
+    request = requests.get(pytest.host + "/api/log/alert", headers=headers)
+    assert request.status_code == 401
+    response = request.json()
+    assert response["error"] == "Bad authentication"
+    assert response["code"] == 401
+    assert response["note"] == "See the documentation https://homeware.enriquegomez.me/api-docs.html"
+
+
+def test_get_alert_fail_no_header_value():
+    headers = {
+        "authorization": f"",
+        "content-type": "application/json"
+    }
+    request = requests.get(pytest.host + "/api/log/alert", headers=headers)
+    assert request.status_code == 401
+    response = request.json()
+    assert response["error"] == "Bad authentication"
+    assert response["code"] == 401
+    assert response["note"] == "See the documentation https://homeware.enriquegomez.me/api-docs.html"
+
+def test_get_alert_fail_no_header():
+    headers = {
+        "content-type": "application/json"
+    }
+    request = requests.get(pytest.host + "/api/log/alert", headers=headers)
+    assert request.status_code == 401
+    response = request.json()
+    assert response["error"] == "Bad authentication"
+    assert response["code"] == 401
+    assert response["note"] == "See the documentation https://homeware.enriquegomez.me/api-docs.html"
