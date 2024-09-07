55b6f092eb62489e45841952a1c9a1297da6ef23
Enrique <enrique.gomez.electronico@gmail.com>
2024-09-07 19:07:51 +0200

    move google auth

--- back/tests_integration/test_api_google.py
+++ back/tests_integration/test_api_google.py
@@ -11,3 +11,41 @@ def test_auth():
     state = "where-is-perry"
     request = requests.get(pytest.host + f"/auth?client_id={client_id}&redirect_uri={redirect_uri}&state={state}")
     assert request.status_code == 404
+
+# Google auth by user /api/user/googleSync
+
+def test_googleSync():
+    headers = {
+        "username": pytest.username,
+        "password": pytest.password
+    }
+    request = requests.get(pytest.host + "/api/user/googleSync", headers=headers)
+    assert request.status_code == 200
+    response = request.json()
+    assert response["status"] == "in"
+    assert response["user"] == pytest.username
+    assert response["url"] != ""
+
+def test_googleSync_fail_bad_username():
+    headers = {
+        "username": "where-is-perry",
+        "password": pytest.password
+    }
+    request = requests.get(pytest.host + "/api/user/googleSync", headers=headers)
+    assert request.status_code == 200
+    response = request.json()
+    assert response["status"] == "fail"
+    assert response["user"] == headers["username"]
+    assert response["url"] == ""
+
+def test_googleSync_fail_bad_password():
+    headers = {
+        "username": pytest.username,
+        "password": "PASSWORD"
+    }
+    request = requests.get(pytest.host + "/api/user/googleSync", headers=headers)
+    assert request.status_code == 200
+    response = request.json()
+    assert response["status"] == "fail"
+    assert response["user"] == pytest.username
+    assert response["url"] == ""
