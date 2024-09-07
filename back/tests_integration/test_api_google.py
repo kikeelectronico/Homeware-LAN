575740a8d585d8e653d6cd49d96bd610ea5d4904
Enrique <enrique.gomez.electronico@gmail.com>
2024-09-07 19:46:12 +0200

    test token end point

--- back/tests_integration/test_api_google.py
+++ back/tests_integration/test_api_google.py
@@ -4,8 +4,10 @@ import json
 import pytest
 
 auth_code = ""
+access_token = ""
+refresh_token = ""
 
-# /auth*
+# /auth
 
 def test_auth():
     client_id = "123"
@@ -53,3 +55,78 @@ def test_googleSync_fail_bad_password():
     assert response["status"] == "fail"
     assert response["user"] == pytest.username
     assert response["url"] == ""
+
+# /token
+
+def generate_form_data_payload(kwargs):
+
+    REQUEST_FORM_DATA_BOUNDARY = "REQUEST_FORM_DATA_BOUNDARY"
+    FORM_DATA_STARTING_PAYLOAD = '--{0}\r\nContent-Disposition: form-data; name=\\"'.format(REQUEST_FORM_DATA_BOUNDARY)
+    FORM_DATA_MIDDLE_PAYLOAD = '\"\r\n\r\n'
+    FORM_DATA_ENDING_PAYLOAD = '--{0}--'.format(REQUEST_FORM_DATA_BOUNDARY)
+    
+
+    payload = ''
+    for key, value in kwargs.items():
+        payload += '{0}{1}{2}{3}\r\n'.format(FORM_DATA_STARTING_PAYLOAD, key, FORM_DATA_MIDDLE_PAYLOAD, value)
+    payload += FORM_DATA_ENDING_PAYLOAD
+    return payload
+
+def test_get_tokens_with_auth_code():
+    body = {
+        "grant_type": "authorization_code",
+        "client_id": "123",
+        "client_secret": "456",
+        "code": auth_code
+    }
+    request = requests.post(pytest.host + f"/token", data=body)
+    assert request.status_code == 200
+    response = request.json()
+    assert response["token_type"] == "bearer"
+    assert response["expires_in"] == 86400
+    assert len(response["access_token"]) == 40
+    assert len(response["refresh_token"]) == 40
+    global access_token
+    access_token = response["access_token"]
+    global refresh_token
+    refresh_token = response["refresh_token"]
+
+def test_get_tokens_with_auth_code_fail_bad_auth_code():
+    body = {
+        "grant_type": "authorization_code",
+        "client_id": "123",
+        "client_secret": "456",
+        "code": "where-is-perry"
+    }
+    request = requests.post(pytest.host + f"/token", data=body)
+    assert request.status_code == 200
+    response = request.json()
+    assert response["error"] == "invalid_grant"
+
+def test_get_tokens_with_refresh_token():
+    body = {
+        "grant_type": "refresh_token",
+        "client_id": "123",
+        "client_secret": "456",
+        "refresh_token": refresh_token
+    }
+    request = requests.post(pytest.host + f"/token", data=body)
+    assert request.status_code == 200
+    response = request.json()
+    assert response["token_type"] == "bearer"
+    assert response["expires_in"] == 86400
+    assert len(response["access_token"]) == 40
+    global access_token
+    access_token = response["access_token"]
+
+def test_get_tokens_with_refresh_token_fail_bad_refresh_token():
+    body = {
+        "grant_type": "authorization_code",
+        "client_id": "123",
+        "client_secret": "456",
+        "refresh_token": "where-is-perry"
+    }
+    request = requests.post(pytest.host + f"/token", data=body)
+    assert request.status_code == 200
+    response = request.json()
+    assert response["error"] == "invalid_grant"
