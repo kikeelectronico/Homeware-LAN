b90e06a70dd3263eec9cf79117bd2eccd6afcb68
Enrique <enrique.gomez.electronico@gmail.com>
2024-09-14 20:49:48 +0200

    test user

---
+++ back/tests/unit_tests/test_data_user.py
@@ -0,0 +1,32 @@
+from data import Data
+
+def test_updatePassword():
+	data = Data()
+	data.setup()
+	password = "admin"
+	new_password = "admin2"
+	assert data.updatePassword(new_password,password) == False
+	assert data.updatePassword(password, new_password) == True
+	assert data.updatePassword(new_password,password) == True
+	del data
+
+def test_login():
+	data = Data()
+	data.setup()
+	username = "admin"
+	password = "admin"
+	assert not data.login(username, password) == None
+	assert data.login("admin2",password) == None
+	assert data.login(username,"admin2") == None
+	del data
+
+def test_validateUserToken():
+	data = Data()
+	data.setup()
+	username = "admin"
+	password = "admin"
+	token = data.login("admin","admin")
+	assert not token == None
+	assert data.validateUserToken(token) == True
+	assert data.validateUserToken("token") == False
+	del data
