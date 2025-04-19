96965e651c29387a4bb71bad8e39ec67e6c065de
Enrique <enrique.gomez.electronico@gmail.com>
2025-04-19 18:25:12 +0200

    update project name

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -19,7 +19,7 @@ const config: Config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'kikeelectronico', // Usually your GitHub org/user name.
-  projectName: 'Homeware', // Usually your repo name.
+  projectName: 'Homeware-LAN', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
