88d69d4cee36a81e949c19e13667839fd77b9bd2
Enrique <enrique.gomez.electronico@gmail.com>
2025-06-15 01:28:25 +0200

    add lock colors

--- front/src/pages/devices/components/traits/LockUnlock.js
+++ front/src/pages/devices/components/traits/LockUnlock.js
@@ -2,34 +2,30 @@ import React, {useState, useEffect}  from 'react';
 
 const LockUnlock = (props) => {
 
-  const [icon, setIcon] = useState("lock_close")
   const [title, setTitle] = useState("")
+  const [color, setColor] = useState("#777")
 
   useEffect(() => {
     if (props.states.isJammed) {
-      setIcon('lock_alert')
-      setTitle('It is jammed')
+      setTitle('Jammed')
+      setColor("#FF5722")
     } else if (props.states.isLocked) {
-      setIcon('lock_close')
-      setTitle('It is close')
+      setTitle('Close')
+      setColor("#4CAF50")
     } else if (!props.states.isLocked) {
-      setIcon('lock_open')
-      setTitle('It is open')
+      setTitle('Open')
+      setColor("#2196F3")
     }
   }, [props.states])
     
-
-    return (
-      <div style={{float: 'left', marginLeft: '5px', height: "30px"}}>
-        <img
-          src={ '/devices/' + icon + '.png' }
-          title={ title }
-          alt={ props.image }
-          style={{width: '30px'}}
-        />
+  return (
+    Object.keys(props.states).includes("isLocked") ?
+      <div>
+        <span style={{color: color}}>{title}</span>
       </div>
-    );
-  
+  : <></>
+  );
+
 }
 
 export default LockUnlock
