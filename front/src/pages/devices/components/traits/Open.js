12a331b71cdd38bb6e5ef4af50deaa1c6f45e436
Enrique <enrique.gomez.electronico@gmail.com>
2025-06-15 01:17:22 +0200

    clean

--- front/src/pages/devices/components/traits/Open.js
+++ front/src/pages/devices/components/traits/Open.js
@@ -1,17 +1,13 @@
 import React from 'react';
 
 const Open = (props) => {
-  
   return (
     Object.keys(props.states).includes("openPercent") ?
       <div>
         {props.states.openPercent} %
       </div>
   : <></>
-    
   );
-  
 }
-
 
 export default Open
