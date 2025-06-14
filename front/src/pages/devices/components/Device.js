99ef76f82fbbd39c215a7e18469c7bb83cdd69c6
Enrique <enrique.gomez.electronico@gmail.com>
2025-06-15 01:37:35 +0200

    set color from lockUnlock

--- front/src/pages/devices/components/Device.js
+++ front/src/pages/devices/components/Device.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, {useState, useEffect}  from 'react'
+
+import LockUnlock from './traits/LockUnlock';
 import OnOff from './traits/OnOff'
 import Open from './traits/Open';
 import TemperatureAmbient from './traits/TemperatureAmbient';
@@ -6,29 +8,44 @@ import TemperatureSetting from './traits/TemperatureSetting';
 import Information from './aux/Information'
 import Connecting from './aux/Connecting'
 import Edit from './aux/Edit'
+import { colors } from '@mui/material';
 
 const Device = (props) => {
 
-  return (
-    <div>
-      <div className="device_card">
-      <div className="device_card_color_strip" style={{backgroundColor: Object.keys(props.states).includes("on") ? "#90EE90" : "white", opacity: props.states.on ? "1" : "0.4"}}></div>
+    const [strip_color, setStripColor] = useState("white")
+    const [strip_on, setStripOn] = useState(true)
 
-        <h2 className="device_card_title">{ props.device.name.name }</h2>
-        <div className="device_card_status">
-            <TemperatureAmbient id={ props.device.id } device={props.device} states={props.states}/>
-            <Open id={ props.device.id } states={props.states}/>
+    useEffect(() => {
+        const states = Object.keys(props.states)
+        if (states.includes("on")) {
+            setStripColor("#90EE90")
+            setStripOn(props.states.on)
+        } else if (states.includes("isLocked")) {
+            setStripOn(true)
+        }
+    }, [props.states])
+
+    return (
+        <div>
+            <div className="device_card">
+                <div className="device_card_color_strip" style={{backgroundColor: strip_color, opacity: strip_on ? "1" : "0.4"}}></div>
+
+                <h2 className="device_card_title">{ props.device.name.name }</h2>
+                <div className="device_card_status">
+                    <LockUnlock id={ props.device.id } states={props.states} setStripColor={setStripColor}/>
+                    <Open id={ props.device.id } states={props.states}/>
+                    <TemperatureAmbient id={ props.device.id } device={props.device} states={props.states}/>
+                </div>
+                
+                <div className="device_card_actions_row">
+                    <OnOff id={ props.device.id } states={props.states} reload={ props.reload }/>
+                    <TemperatureSetting id={ props.device.id } states={props.states} reload={ props.reload }/>
+                    
+                    <Edit id={ props.device.id }/>
+                </div>
+            </div>
         </div>
-        
-        <div className="device_card_actions_row">
-            <OnOff id={ props.device.id } states={props.states} reload={ props.reload }/>
-            <TemperatureSetting id={ props.device.id } states={props.states} reload={ props.reload }/>
-            
-            <Edit id={ props.device.id }/>
-        </div>
-      </div>
-    </div>
-  );
+    );
   
 }
 
--- front/src/pages/devices/components/traits/LockUnlock.js
+++ front/src/pages/devices/components/traits/LockUnlock.js
@@ -6,15 +6,20 @@ const LockUnlock = (props) => {
   const [color, setColor] = useState("#777")
 
   useEffect(() => {
-    if (props.states.isJammed) {
-      setTitle('Jammed')
-      setColor("#FF5722")
-    } else if (props.states.isLocked) {
-      setTitle('Close')
-      setColor("#4CAF50")
-    } else if (!props.states.isLocked) {
-      setTitle('Open')
-      setColor("#2196F3")
+    if (Object.keys(props.states).includes("isLocked")) {
+      if (props.states.isJammed) {
+        setTitle('Jammed')
+        setColor("#FF5722")
+        props.setStripColor("#FF5722")
+      } else if (props.states.isLocked) {
+        setTitle('Close')
+        setColor("#4CAF50")
+        props.setStripColor("#4CAF50")
+      } else if (!props.states.isLocked) {
+        setTitle('Open')
+        setColor("#2196F3")
+        props.setStripColor("#2196F3")
+      }
     }
   }, [props.states])
     
