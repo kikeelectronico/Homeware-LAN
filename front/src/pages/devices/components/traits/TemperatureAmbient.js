c419143cc63fa4799a9c746e479ff9dac5ddaba2
Enrique <enrique.gomez.electronico@gmail.com>
2025-06-15 21:24:28 +0200

    add temperatureAmbientCelsius state

--- front/src/pages/devices/components/traits/TemperatureAmbient.js
+++ front/src/pages/devices/components/traits/TemperatureAmbient.js
@@ -3,11 +3,22 @@ import React from "react";
 const TemperatureAmbient = (props) => {
 
   return (
-    Object.keys(props.states).includes("thermostatTemperatureAmbient") ?
-      <div className="device_card_status">
-        {props.states.thermostatTemperatureAmbient} {props.device.attributes.thermostatTemperatureUnit === "C" ? "ºC" : "F"}
-      </div>
-    : <></>
+    <>
+      {
+        Object.keys(props.states).includes("thermostatTemperatureAmbient") ?
+          <div className="device_card_status">
+            {props.states.thermostatTemperatureAmbient} {props.device.attributes.thermostatTemperatureUnit === "C" ? "ºC" : "F"}
+          </div>
+        : <></>
+      }
+      {
+        Object.keys(props.states).includes("temperatureAmbientCelsius") ?
+          <div className="device_card_status">
+            {props.states.temperatureAmbientCelsius} {props.device.attributes.temperatureUnitForUX === "C" ? "ºC" : "F"}
+          </div>
+        : <></>
+      }
+    </>
   )
 
 }
