ac355e3842a88a66266b5bb563a8113ffc1b556f
kikeelectronico <hola@enriquegomez.me>
2026-05-17 01:38:21 +0200

    multiple sensors support

--- front/src/components/editor/traits/SensorState.jsx
+++ front/src/components/editor/traits/SensorState.jsx
@@ -1,5 +1,6 @@
 import React, {useEffect, forwardRef, useImperativeHandle, useState} from 'react';
-import {Select, MenuItem} from '@mui/material';
+import {InputLabel, Button, Select, MenuItem, Box, FormControl, IconButton, Stack} from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 const sensorCapabilities = {
   AirQuality: {
@@ -65,11 +66,17 @@ const sensorCapabilities = {
 }
 
 const attributes = {
-  sensorStatesSupported: [{name: ""}],
+  sensorStatesSupported: [{name: "AirQuality", ...sensorCapabilities.AirQuality}],
 }
 
 const states = {
-  currentSensorStateData: [],
+  currentSensorStateData: [
+    {
+      name: "AirQuality",
+      currentSensorState: "healthy",
+      rawValue: 0
+    }
+  ],
 }
 
 const SensorState = forwardRef((props, ref) => {
@@ -93,10 +100,10 @@ const SensorState = forwardRef((props, ref) => {
   }))
 
   const getDefaultSensorState = (name) => {
-    if (!name) return []
-
     const sensor = sensorCapabilities[name]
-    const state = {name}
+    const state = {
+      name: name
+    }
 
     if (sensor.descriptiveCapabilities) {
       state.currentSensorState = sensor.descriptiveCapabilities.availableStates[0]
@@ -106,47 +113,98 @@ const SensorState = forwardRef((props, ref) => {
       state.rawValue = 0
     }
 
-    return [state]
+    return state
   }
 
-  const updateSensorType = (value) => {
-    const _sensorStatesSupported = value ? [{name: value, ...sensorCapabilities[value]}] : attributes.sensorStatesSupported
+  const getCurrentSensorStateData = (_sensorStatesSupported) => {
+    return _sensorStatesSupported.map((sensor) => getDefaultSensorState(sensor.name))
+  }
+
+  const addSensor = () => {
+    let _sensorStatesSupported = [...sensorStatesSupported]
+    _sensorStatesSupported.push({name: "AirQuality", ...sensorCapabilities.AirQuality})
     setSensorStatesSupported(_sensorStatesSupported)
     props.updateAttributes("sensorStatesSupported", _sensorStatesSupported, "update")
-    props.updateStates("currentSensorStateData", getDefaultSensorState(value), "update")
+    props.updateStates("currentSensorStateData", getCurrentSensorStateData(_sensorStatesSupported), "update")
+  }
+
+  const removeSensor = (index) => {
+    let _sensorStatesSupported = [...sensorStatesSupported]
+    _sensorStatesSupported.splice(index, 1)
+    setSensorStatesSupported(_sensorStatesSupported)
+    props.updateAttributes("sensorStatesSupported", _sensorStatesSupported, "update")
+    props.updateStates("currentSensorStateData", getCurrentSensorStateData(_sensorStatesSupported), "update")
+  }
+
+  const updateSensorType = (index, value) => {
+    let _sensorStatesSupported = [...sensorStatesSupported]
+    _sensorStatesSupported[index] = {name: value, ...sensorCapabilities[value]}
+    setSensorStatesSupported(_sensorStatesSupported)
+    props.updateAttributes("sensorStatesSupported", _sensorStatesSupported, "update")
+    props.updateStates("currentSensorStateData", getCurrentSensorStateData(_sensorStatesSupported), "update")
   }
 
   return (
     <div className="attributes_table">
       <div className="attributes_row">
-        <div className="attributes_col_1 attributes_label align_right">
-          Sensor type
+        <div className="attributes_col_1 attributes_label align_right attributes_col_allign_up">
+          <i>sensorStatesSupported</i>
         </div>
-        <div className="attributes_col_2">
-          <Select
-            name="type"
-            id="sensorStatesSupported"
-            className="table_input"
-            value={sensorStatesSupported[0]?.name || ""}
-            displayEmpty
-            onChange={(event) => updateSensorType(event.target.value)}
-          >
-            <MenuItem value="">Select</MenuItem>
-            <MenuItem value="AirQuality">Air Quality</MenuItem>
-            <MenuItem value="CarbonDioxideLevel">Carbon Dioxide Level</MenuItem>
-            <MenuItem value="CarbonMonoxideLevel">Carbon Monoxide Level</MenuItem>
-            <MenuItem value="FilterCleanliness">Filter Cleanliness</MenuItem>
-            <MenuItem value="FilterLifeTime">Filter Life Time</MenuItem>
-            <MenuItem value="HEPAFilterLifeTime">HEPA Filter Life Time</MenuItem>
-            <MenuItem value="Max2FilterLifeTime">Max2 Filter Life Time</MenuItem>
-            <MenuItem value="PM2.5">PM2.5</MenuItem>
-            <MenuItem value="PM10">PM10</MenuItem>
-            <MenuItem value="PreFilterLifeTime">PreFilter Life Time</MenuItem>
-            <MenuItem value="SmokeLevel">Smoke Level</MenuItem>
-            <MenuItem value="WaterLeak">Water Leak</MenuItem>
-            <MenuItem value="RainDetection">Rain Detection</MenuItem>
-            <MenuItem value="VolatileOrganicCompounds">Volatile Organic Compounds</MenuItem>
-          </Select>
+        <div className="attributes_col_2 attribute_table_form">
+          {
+            sensorStatesSupported.map((sensor, index) => {
+              return (
+                <Box className="attribute_table_subattribute" key={index}>
+                  <Box className="attribute_table_subattribute_row">
+                    <FormControl fullWidth>
+                      <InputLabel id={"sensorStateType-label-" + index}>
+                        Sensor type
+                      </InputLabel>
+                      <Select
+                        id={"sensorStateType-" + index}
+                        data-test="sensorStateType"
+                        label="Sensor type"
+                        className="attribute_table_subattribute_input"
+                        value={sensor.name}
+                        onChange={(event) => {
+                          updateSensorType(index, event.target.value)
+                        }}
+                      >
+                        <MenuItem value="AirQuality">Air Quality</MenuItem>
+                        <MenuItem value="CarbonDioxideLevel">Carbon Dioxide Level</MenuItem>
+                        <MenuItem value="CarbonMonoxideLevel">Carbon Monoxide Level</MenuItem>
+                        <MenuItem value="FilterCleanliness">Filter Cleanliness</MenuItem>
+                        <MenuItem value="FilterLifeTime">Filter Life Time</MenuItem>
+                        <MenuItem value="HEPAFilterLifeTime">HEPA Filter Life Time</MenuItem>
+                        <MenuItem value="Max2FilterLifeTime">Max2 Filter Life Time</MenuItem>
+                        <MenuItem value="PM2.5">PM2.5</MenuItem>
+                        <MenuItem value="PM10">PM10</MenuItem>
+                        <MenuItem value="PreFilterLifeTime">PreFilter Life Time</MenuItem>
+                        <MenuItem value="SmokeLevel">Smoke Level</MenuItem>
+                        <MenuItem value="WaterLeak">Water Leak</MenuItem>
+                        <MenuItem value="RainDetection">Rain Detection</MenuItem>
+                        <MenuItem value="VolatileOrganicCompounds">Volatile Organic Compounds</MenuItem>
+                      </Select>
+                    </FormControl>
+                  </Box>
+                  <Stack direction="row" spacing={1}>
+                    <IconButton size="large" onClick={() => removeSensor(index)}>
+                      <DeleteIcon />
+                    </IconButton>
+                  </Stack>
+                </Box>
+              )
+            })
+          }
+          <Box className="attribute_table_form_add_button">
+            <Button
+              variant="contained"
+              className="attribute_table_form_add_button"
+              onClick={addSensor}
+            >
+              Add
+            </Button>
+          </Box>
         </div>
       </div>
     </div>
