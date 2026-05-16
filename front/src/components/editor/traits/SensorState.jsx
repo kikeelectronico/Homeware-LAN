45b7b974726e71c67e791a0e2ae99bf7cad91f1c
kikeelectronico <hola@enriquegomez.me>
2026-05-17 01:24:37 +0200

    migrate SensorState

--- front/src/components/editor/traits/SensorState.jsx
+++ front/src/components/editor/traits/SensorState.jsx
@@ -1,124 +1,156 @@
-import React from 'react';
+import React, {useEffect, forwardRef, useImperativeHandle, useState} from 'react';
+import {Select, MenuItem} from '@mui/material';
 
-class SensorState extends React.Component {
-  constructor(props) {
-    super(props);
-    this.update = this.update.bind(this);
-  }
-
-  update(event){
-    const value = event.target.value;
-    var sensor = {
-      name: value
-    }
-
-    switch (value) {
-      default:
-        sensor['descriptiveCapabilities'] = {
-          availableStates: ['fair','good','healthy','moderate','poor','severe','unhealthy','unhealthy for sensitive groups','very poor','very unhealthy','unknown'],
-        }
-        sensor['numericCapabilities']= { rawValueUnit: "AQI" }
-        break;
-      case 'AirQuality':
-        sensor['descriptiveCapabilities'] = {
-          availableStates: ['fair','good','healthy','moderate','poor','severe','unhealthy','unhealthy for sensitive groups','very poor','very unhealthy','unknown'],
-        }
-        sensor['numericCapabilities']= { rawValueUnit: "AQI" }
-        break;
-      case 'CarbonDioxideLevel':
-        sensor['descriptiveCapabilities'] = {
-          numericCapabilities: { rawValueUnit: "PARTS_PER_MILLION" }
-        }
-        break;
-      case 'CarbonMonoxideLevel':
-        sensor['descriptiveCapabilities'] = {
-          availableStates: ['carbon monoxide detected','high','no carbon monoxide detected','unknown']
-        }
-        sensor['numericCapabilities']= { rawValueUnit: "PARTS_PER_MILLION" }
-        break;
-      case 'FilterCleanliness':
-        sensor['descriptiveCapabilities'] = {
-          availableStates: ['clean','dirty','needs replacement','unknown']
-        }
-        break;
-      case 'FilterLifeTime':
-        sensor['descriptiveCapabilities'] = {
-          availableStates: ['good','new','replace soon','replace now','unknown']
-        }
-        sensor['numericCapabilities']= { rawValueUnit: "PERCENTAGE" }
-        break;
-      case 'HEPAFilterLifeTime':
-        sensor['numericCapabilities']= { rawValueUnit: "PERCENTAGE" }
-        break;
-      case 'Max2FilterLifeTime':
-        sensor['numericCapabilities']= { rawValueUnit: "PERCENTAGE" }
-        break;
-      case 'PM2.5':
-        sensor['numericCapabilities']= { rawValueUnit: "MICROGRAMS_PER_CUBIC_METER" }
-        break;
-      case 'PM10':
-        sensor['numericCapabilities']= { rawValueUnit: "MICROGRAMS_PER_CUBIC_METER" }
-        break;
-      case 'PreFilterLifeTime':
-        sensor['numericCapabilities']= { rawValueUnit: "PERCENTAGE" }
-        break;
-      case 'SmokeLevel':
-        sensor['descriptiveCapabilities'] = {
-          availableStates: ['smoke detected','high','no smoke detected','unknown']
-        }
-        sensor['numericCapabilities']= { rawValueUnit: "PARTS_PER_MILLION" }
-        break;
-      case 'WaterLeak':
-        sensor['descriptiveCapabilities'] = {
-          availableStates: ['leak','no leak','unknown']
-        }
-        break;
-      case 'RainDetection':
-        sensor['descriptiveCapabilities'] = {
-          availableStates: ['rain detected','no rain detected','unknown']
-        }
-        break;
-      case 'VolatileOrganicCompounds':
-        sensor['numericCapabilities']= { rawValueUnit: "PARTS_PER_MILLION" }
-        break;
-    }
-
-    var sensorStatesSupported = []
-    sensorStatesSupported.push(sensor);
-    this.props.update('attributes/' + event.target.id,sensorStatesSupported);
-  }
-
-  render() {
-    return (
-      <div>
-        <div className="two_table_row">
-          <div className="two_table_cel two_table_label align_right">
-            Sensor type
-          </div>
-          <div className="two_table_cel">
-            <select name="type" id="sensorStatesSupported" className="table_input" value={this.props.attributes.sensorStatesSupported[0].name} onChange={this.update}>
-              <option value="">Select</option>
-              <option value="AirQuality">Air Quality</option>
-              <option value="CarbonDioxideLevel">Carbon Dioxide Level</option>
-              <option value="CarbonMonoxideLevel">Carbon Monoxide Level</option>
-              <option value="FilterCleanliness">Filter Clean liness</option>
-              <option value="FilterLifeTime">Filter Life Time</option>
-              <option value="HEPAFilterLifeTime">HEPA Filter Life Time</option>
-              <option value="Max2FilterLifeTime">Max2 Filter Life Time</option>
-              <option value="PM2.5">PM2.5</option>
-              <option value="PM10">PM10</option>
-              <option value="PreFilterLifeTime">PreFilter Life Time</option>
-              <option value="SmokeLevel">Smoke Level</option>
-              <option value="WaterLeak">Water Leak</option>
-              <option value="RainDetection">Rain Detection</option>
-              <option value="VolatileOrganicCompounds">Volatile Organic Compounds</option>
-            </select>
-          </div>
-        </div>
-
-      </div>
-    );
-  }
+const sensorCapabilities = {
+  AirQuality: {
+    descriptiveCapabilities: {
+      availableStates: ["healthy", "moderate", "unhealthy", "unhealthy for sensitive groups", "very unhealthy", "hazardous", "good", "fair", "poor", "very poor", "severe", "unknown"],
+    },
+    numericCapabilities: {rawValueUnit: "AQI"},
+  },
+  CarbonDioxideLevel: {
+    numericCapabilities: {rawValueUnit: "PARTS_PER_MILLION"},
+  },
+  CarbonMonoxideLevel: {
+    descriptiveCapabilities: {
+      availableStates: ["carbon monoxide detected", "high", "no carbon monoxide detected", "unknown"],
+    },
+    numericCapabilities: {rawValueUnit: "PARTS_PER_MILLION"},
+  },
+  FilterCleanliness: {
+    descriptiveCapabilities: {
+      availableStates: ["clean", "dirty", "needs replacement", "unknown"],
+    },
+  },
+  FilterLifeTime: {
+    descriptiveCapabilities: {
+      availableStates: ["new", "good", "replace soon", "replace now", "unknown"],
+    },
+    numericCapabilities: {rawValueUnit: "PERCENTAGE"},
+  },
+  HEPAFilterLifeTime: {
+    numericCapabilities: {rawValueUnit: "PERCENTAGE"},
+  },
+  Max2FilterLifeTime: {
+    numericCapabilities: {rawValueUnit: "PERCENTAGE"},
+  },
+  "PM2.5": {
+    numericCapabilities: {rawValueUnit: "MICROGRAMS_PER_CUBIC_METER"},
+  },
+  PM10: {
+    numericCapabilities: {rawValueUnit: "MICROGRAMS_PER_CUBIC_METER"},
+  },
+  PreFilterLifeTime: {
+    numericCapabilities: {rawValueUnit: "PERCENTAGE"},
+  },
+  SmokeLevel: {
+    descriptiveCapabilities: {
+      availableStates: ["smoke detected", "high", "no smoke detected", "unknown"],
+    },
+    numericCapabilities: {rawValueUnit: "PARTS_PER_MILLION"},
+  },
+  WaterLeak: {
+    descriptiveCapabilities: {
+      availableStates: ["leak", "no leak", "unknown"],
+    },
+  },
+  RainDetection: {
+    descriptiveCapabilities: {
+      availableStates: ["rain detected", "no rain detected", "unknown"],
+    },
+  },
+  VolatileOrganicCompounds: {
+    numericCapabilities: {rawValueUnit: "PARTS_PER_MILLION"},
+  },
 }
+
+const attributes = {
+  sensorStatesSupported: [{name: ""}],
+}
+
+const states = {
+  currentSensorStateData: [],
+}
+
+const SensorState = forwardRef((props, ref) => {
+
+  const [sensorStatesSupported, setSensorStatesSupported] = useState(attributes.sensorStatesSupported)
+
+  useEffect(() => {
+    if ("sensorStatesSupported" in props.attributes) {
+      setSensorStatesSupported(props.attributes.sensorStatesSupported)
+    } else {
+      props.updateStates(null, states, "insert")
+      props.updateAttributes(null, attributes, "insert")
+    }
+  }, [props])
+
+  useImperativeHandle(ref, () => ({
+    deleteAttributes() {
+      props.updateStates(null, states, "drop")
+      props.updateAttributes(null, attributes, "drop")
+    }
+  }))
+
+  const getDefaultSensorState = (name) => {
+    if (!name) return []
+
+    const sensor = sensorCapabilities[name]
+    const state = {name}
+
+    if (sensor.descriptiveCapabilities) {
+      state.currentSensorState = sensor.descriptiveCapabilities.availableStates[0]
+    }
+
+    if (sensor.numericCapabilities) {
+      state.rawValue = 0
+    }
+
+    return [state]
+  }
+
+  const updateSensorType = (value) => {
+    const _sensorStatesSupported = value ? [{name: value, ...sensorCapabilities[value]}] : attributes.sensorStatesSupported
+    setSensorStatesSupported(_sensorStatesSupported)
+    props.updateAttributes("sensorStatesSupported", _sensorStatesSupported, "update")
+    props.updateStates("currentSensorStateData", getDefaultSensorState(value), "update")
+  }
+
+  return (
+    <div className="attributes_table">
+      <div className="attributes_row">
+        <div className="attributes_col_1 attributes_label align_right">
+          Sensor type
+        </div>
+        <div className="attributes_col_2">
+          <Select
+            name="type"
+            id="sensorStatesSupported"
+            className="table_input"
+            value={sensorStatesSupported[0]?.name || ""}
+            displayEmpty
+            onChange={(event) => updateSensorType(event.target.value)}
+          >
+            <MenuItem value="">Select</MenuItem>
+            <MenuItem value="AirQuality">Air Quality</MenuItem>
+            <MenuItem value="CarbonDioxideLevel">Carbon Dioxide Level</MenuItem>
+            <MenuItem value="CarbonMonoxideLevel">Carbon Monoxide Level</MenuItem>
+            <MenuItem value="FilterCleanliness">Filter Cleanliness</MenuItem>
+            <MenuItem value="FilterLifeTime">Filter Life Time</MenuItem>
+            <MenuItem value="HEPAFilterLifeTime">HEPA Filter Life Time</MenuItem>
+            <MenuItem value="Max2FilterLifeTime">Max2 Filter Life Time</MenuItem>
+            <MenuItem value="PM2.5">PM2.5</MenuItem>
+            <MenuItem value="PM10">PM10</MenuItem>
+            <MenuItem value="PreFilterLifeTime">PreFilter Life Time</MenuItem>
+            <MenuItem value="SmokeLevel">Smoke Level</MenuItem>
+            <MenuItem value="WaterLeak">Water Leak</MenuItem>
+            <MenuItem value="RainDetection">Rain Detection</MenuItem>
+            <MenuItem value="VolatileOrganicCompounds">Volatile Organic Compounds</MenuItem>
+          </Select>
+        </div>
+      </div>
+    </div>
+  );
+})
 
 export default SensorState
